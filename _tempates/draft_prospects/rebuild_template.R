1c7a579d856217a51ac5d9dd49f5bab61372f82d
t-gummer <timothy.gummer@gmail.com>
2023-11-11 17:08:59 +1100

    update website

---
+++ _tempates/draft_prospects/rebuild_template.R
@@ -0,0 +1,75 @@
+library(arrow)
+library(dplyr)
+library(stringr)
+library(fs)
+library(quarto)
+
+quarto_template <- readLines("_tempates/draft_prospects/template.qmd")
+male_folder_path <- "players/afl/underage_profiles/"
+female_folder_path <- "players/aflw/underage_profiles/"
+
+combine_player_details <- read_parquet("https://github.com/bit-in-that/data-automation/raw/main/players/data/processed/combine_player_details.parquet")
+combine_player_seasons <- read_parquet("https://github.com/bit-in-that/data-automation/raw/main/players/data/processed/combine_player_seasons.parquet")
+combine_player_stats <- read_parquet("https://github.com/bit-in-that/data-automation/raw/main/players/data/processed/combine_player_stats.parquet")
+
+season_data_table <- combine_player_seasons |> 
+  select(-tier_short) |> 
+  relocate(
+    season, .before = "tier"
+  ) |> 
+  arrange(
+    desc(season), desc(games_played)
+  )
+
+match_data_table <- combine_player_stats |> 
+  relocate(
+    match_time, .before = "tier"
+  ) |> 
+  mutate(
+    comp_name = paste(comp_name, season)
+  ) |> 
+  select(
+    -season, -player_first_name, -player_surname, -match_id, -(inside50s:handballs_received)
+  ) |> 
+  arrange(desc(match_time))
+
+write_parquet(combine_player_details, "_tempates/draft_prospects/combine_player_details.parquet")
+write_parquet(season_data_table, "_tempates/draft_prospects/season_data_table.parquet")
+write_parquet(match_data_table, "_tempates/draft_prospects/match_data_table.parquet")
+
+
+
+{
+  file_delete(dir_ls(male_folder_path))
+  file_delete(dir_ls(female_folder_path))
+  
+  combine_player_details |> 
+    mutate(
+      first_playerId = map_chr(playerIds, head, n = 1)
+    ) |> 
+    arrange(phantom_draft_afl) |> 
+    head(10) |> 
+  with({
+    pwalk(list(first_playerId, player_first_name, player_surname, gender), \(id, first_name, surname, gender){
+      if(gender == "male") {
+        folder_path <- male_folder_path
+      } else {
+        folder_path <- female_folder_path
+        
+      }
+      
+      quarto_template |> 
+        str_replace_all(fixed("||player_name||"), paste(first_name, surname)) |> 
+        str_replace_all(fixed("||player_first_name||"), first_name) |> 
+        str_replace_all(fixed("||player_surname||"), surname) |> 
+        writeLines(paste0(folder_path, id, ".qmd"))
+    })
+  })
+}
+
+dir_delete("_freeze/players/afl/underage_profiles/")
+dir_delete("_freeze/players/aflw/underage_profiles/")
+
+quarto_render("players/afl/underage_profiles")
+quarto_render("players/aflw/underage_profiles")
+
--- dev/renv_history.R
+++ dev/renv_history.R
@@ -7,6 +7,7 @@ renv::install("xml2")
 renv::install("crosstalk")
 library(downlit)
 library(xml2)
+library(crosstalk)
 
 renv::snapshot()
 
