b6066853fdfba459ecde2217c1f97a36d2beedd2
t-gummer <timothy.gummer@gmail.com>
2024-02-20 12:29:39 +0800

    add player profile

--- dev/renv_history.R
+++ dev/renv_history.R
@@ -5,9 +5,11 @@ renv::init()
 renv::install("downlit")
 renv::install("xml2")
 renv::install("crosstalk")
+renv::install("tzdb")
 library(downlit)
 library(xml2)
 library(crosstalk)
+library(tzdb)
 
 renv::snapshot()
 
